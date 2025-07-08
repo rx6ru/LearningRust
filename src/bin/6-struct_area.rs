f65ba4b428950db121079f64a8710cddce624525
rxbru <amarmahato.cs@gmail.com>
2025-07-09 01:42:15 +0530

    more on structs

---
+++ src/bin/6-struct_area.rs
@@ -0,0 +1,23 @@
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn perimeter(&self) -> u32 {
+        2 * (self.width + self.height)
+    }
+}
+
+fn main() {
+    let rect1 = Rect {
+        width: 30,
+        height: 50,
+    };
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+    println!("The perimeter of the rectangle is {} pixels.", rect1.perimeter());
+}
